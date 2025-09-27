81bd69599f7ba8f4a37a5e00f70250b560a5d1d6
Aniket Tikudave <anikettikudave187@gmail.com>
2025-09-27 18:34:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+document.getElementById("myForm").addEventListener("submit",function(e){
+	e.preventDefault();
+
+	const baseUrl = "https://localhost:8080/";
+	const name=document.getElementById("name").value.trim();
+	const year=document.getElementById("year").value.trim();
+
+	let queryParams=[];
+
+	if(name){
+		queryParams.push(`name=${encodeURIComponent(name)}`);
+	}
+	if(year){
+		queryParams.push(`year=${encodeURIComponent(year)}`);
+	}
+
+	const finalUrl=
+	queryParams.length > 0 ? baseUrl + "?" + queryParams.join("&") : baseUrl;
+
+	document.getElementById("url").textContent = finalUrl;
+});
